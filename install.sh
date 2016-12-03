45d94b96674062feaeac8e9d2c350b7ae6ab1c5e
enukane <enukane@glenda9.org>
2016-12-03 12:36:20 +0900

    init
    Signed-off-by: enukane <enukane@glenda9.org>

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+PORTAL_URL=$1
+
+BIN_TARGET=/usr/sbin/pitft-kiosk-start
+PROFILE_PATH=/home/pi/.profile
+
+cat << EOS > $BIN_TARGET
+#!/bin/sh
+
+xset -dpms
+xset s off
+matchbox-window-manager &
+midori -e Fullscreen -a $PORTAL_URL
+EOS
+
+chmod +x /usr/sbin/pitft-kiosk-start
+
+cat << EOS >> $PROFILE_PATH
+
+f [ $TERM = "linux" ]; then
+	# pitft := "linux", ssh := "vt100"
+	xinit $BIN_TARGET
+fi
+EOS
